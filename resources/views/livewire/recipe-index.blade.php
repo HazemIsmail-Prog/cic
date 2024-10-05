701471e87ba27b1cb96bfa233e2b25bb4186ef35
Hazem <hazem.ismail@hotmail.com>
2024-10-05 11:08:11 +0300

    fix responsive margins

--- resources/views/livewire/recipe-index.blade.php
+++ resources/views/livewire/recipe-index.blade.php
@@ -5,7 +5,7 @@
 </x-slot>
 
 <div>
-    <div class=" flex items-center justify-between mb-3">
+    <div class="px-4 sm:px-0 flex items-center justify-between mb-4">
 
         <a wire:navigate
             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
@@ -77,5 +77,5 @@
             </div>
         </div>
     </div>
-    <div class="mt-3">{{ $this->recipes->links() }}</div>
+    <div class="px-4 sm:px-0 mt-4">{{ $this->recipes->links() }}</div>
 </div>
--- resources/views/livewire/user-index.blade.php
+++ resources/views/livewire/user-index.blade.php
@@ -6,7 +6,7 @@
 
 
 <div>
-    <div class=" flex items-center justify-between mb-3">
+    <div class="px-4 sm:px-0 flex items-center justify-between mb-4">
 
         <a wire:navigate
             class="inline-flex users-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
@@ -49,5 +49,5 @@
             </div>
         </div>
     </div>
-    <div class="mt-3">{{ $this->users->links() }}</div>
+    <div class="px-4 sm:px-0 mt-4">{{ $this->users->links() }}</div>
 </div>
